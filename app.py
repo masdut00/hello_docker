5659a44569e5e7ee64fd120b60acd5b29bf02dfa
masdut00 <dutarifki14@gmail.com>
2024-04-03 12:26:57 +0700

    first

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.get('/')
 def route_hello():
-    return 'Hello World'
+    return render_template("index.html")
 
 app.run(host='0.0.0.0', port=8081)
